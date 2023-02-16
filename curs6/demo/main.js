40e38f9463dcfddb8e41e42fba275a5210280cd2
Frontend JavaScript <frontendjavascript@C3-24.local>
2023-02-16 20:54:59 +0200

    commit cu saas

---
+++ curs6/demo/main.js
@@ -0,0 +1,14 @@
+ function add (a, b){
+    return a + b;
+}
+
+ function inm(a,b){
+    return a * b;
+}
+
+ function salut(n){
+    return `buna ziua ${n}`;
+}
+
+export default salut
+export {add, inm}
